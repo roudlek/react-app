068f0ffa4e678e058ea83198e157cf9de64baa83
aziz <elidrissiabdalaziz@gmail.com>
2023-08-26 19:51:43 +0100

    added some components

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-
+import Home from '../pages/index'
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+      {/* <Home /> */}
+      <Main />
+      <NextScript />
       </body>
     </Html>
   )
